faa21c32e5b2afb055bb36ed02e8c5ccb9915fc3
Sparsh Agarwal <sparshagarwal205@gmail.com>
2018-10-16 23:36:31 +0530

    udpates made

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-EMAIL='16ucc094@lnmiit.ac.in'
-PASSWORD='passwordhai'
-DATABASE_HOST='localhost'
-DATABASE_USER='root'
-DATABASE_PASSWORD='haipassword'
-MYSQL_DATABASE_NAME='imdb'
+EMAIL='YOUR_EMAIL' #Enter your email id from which you want to send mail
+PASSWORD='YOUR_EMAIL_PASSWORD' # Password of that email id
+DATABASE_HOST='MYSQL_DATABASE_HOST' # Mysql database host
+DATABASE_USER='MYSQL_DATABASE_USER' # Mysql database user
+DATABASE_PASSWORD='MYSQL_DATABASE_PASSWORD' # mysql database password
